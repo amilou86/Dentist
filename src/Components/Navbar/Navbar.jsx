3c9691163f9f013b7150be1534f66e7667cd85a4
amilou86 <98551756+amilou86@users.noreply.github.com>
2024-06-14 18:56:57 +0100

    sticky navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import './Navbar.css';
 import HRDlogo1 from '../../assets/HRDlogo1.png';
@@ -6,32 +6,43 @@ import HRDlogo2 from '../../assets/HRDlogo2.png'
 import HRDlogo3 from '../../assets/HRDlogo3.png'
 import HRDlogo4 from '../../assets/HRDlogo4.png'
 
-const Navbar = () => (
-    <nav className='container'>
-        <img src={HRDlogo4} alt="logo" className='logo' />
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/About">About</Link>
-            </li>
-            <li>
-                <Link to="/Services">Services</Link>
-            </li>
-            <li>
-                <Link to="/Application Form">Application Form</Link>
-            </li>
-            <li>
-                <Link to="/FeesFinance">Fees & Finance</Link>
-            </li>
-            <li><button className='btn'>Contact Us</button>
-                <Link to="/Contact"></Link>
-            </li>
-        </ul>
+const Navbar = () => {
 
-    </nav>
-)
+    const [sticky, setSticky] = useState(false);
 
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            window.scrollY > 50 ? setSticky(true) : setSticky(false)
+        })
+    }, [])
+
+    return (
+        < nav className={`container ${sticky ? 'dark-nav' : ''}`} >
+            <img src={HRDlogo4} alt="logo" className='logo' />
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/About">About</Link>
+                </li>
+                <li>
+                    <Link to="/Services">Services</Link>
+                </li>
+                <li>
+                    <Link to="/Application Form">Application Form</Link>
+                </li>
+                <li>
+                    <Link to="/FeesFinance">Fees & Finance</Link>
+                </li>
+                <li><button className='btn'>Contact Us</button>
+                    <Link to="/Contact"></Link>
+                </li>
+            </ul>
+
+        </nav >
+    )
+
+}
 
 export default Navbar
